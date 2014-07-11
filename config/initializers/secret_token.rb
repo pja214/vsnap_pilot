aea58a9f577e3325321bda426865b8dfae3a256b
Patrick Adduci <adduci92@gmail.com>
2014-07-11 09:04:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vsnap::Application.config.secret_key_base = '87ea101cb72f54acff94bdda21ef112262084413a7e167977b7b6646535ad7b541b9b68955f5de3230cd956cd32488153ca9774cdf7f6f8f56a5e90078d169f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vsnap::Application.config.session_store :cookie_store, key: '_vsnap_session'
